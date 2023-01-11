1de2ed2b92683fbd7c783fa63c9c3781037accc6
dario-nihil <dario.nihil@gmail.com>
2023-01-11 14:46:41 +0100

    implemented route protection for Profile Page in serverSideProps

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,26 @@
+import { getToken } from "next-auth/jwt";
+
 import UserProfile from "../components/profile/user-profile";
 
 function ProfilePage() {
   return <UserProfile />;
 }
+
+export const getServerSideProps = async (context) => {
+  const token = await getToken({ req: context.req });
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 export default ProfilePage;
