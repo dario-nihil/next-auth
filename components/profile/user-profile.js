6811baef0056d4ece2d3d556b2062aabc0cdbf33
dario-nihil <dario.nihil@gmail.com>
2023-01-11 14:17:29 +0100

    implemented route protection on the profile page

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,22 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
 import ProfileForm from "./profile-form";
 import styles from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p className={styles.profile}>Loading...</p>;
+  }
+
+  if (status === "unauthenticated") {
+    router.replace("/auth");
+    return;
+  }
 
   return (
     <section className={styles.profile}>
